7874fef61e84dadc77210b27156a014c60b84176
Pedro Casseb <casseb.phcc@gmail.com>
2026-05-10 15:31:27 -0300

    implements macbook product viewer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { ScrollTrigger } from "gsap/all";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ProductViwer from "./components/ProductViewer";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
     return (
         <>
             <Navbar />
             <Hero />
+            <ProductViwer />
         </>
     );
 }
